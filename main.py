857c28e0d7f6861d8237afb0204df73b342e9a25
jwmillssr <jwmillssr@yahoo.com>
2023-03-16 14:49:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+# main.py
+
 from fastapi import FastAPI
+import pandas as pd
+from openpyxl import Workbook
+import csv
+
+labelRepoFile = 'Label_Repo.xlsx'
+df3 = pd.read_excel(labelRepoFile, sheet_name='Processing')
+
+myDict = df3.to_dict('records')
+
+
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/rei/")
+async def rei(regnum: str, dateval: str):
+    rei = [x['REI'] for x in myDict if x['EPA Reg. No.'] == regnum and x['Label Date'] == dateval]
+    return {"REI": rei}
+
+@app.get("/ppe/")
+async def rei(regnum: str, dateval: str):
+    ppe = [x['PPE'] for x in myDict if x['EPA Reg. No.'] == regnum and x['Label Date'] == dateval]
+    return {"PPE": ppe}
